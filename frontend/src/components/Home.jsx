9a429c65159eb9755e914599999979ef2aa0981e
tmshts <tomhatas@gmail.com>
2024-07-07 23:14:47 -0500

    add image slider

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,48 @@
+import ImageSlider from './ImageSlider'
+
+import Blog_blogs_foto from '../assets/1_Blog_blogs.png'
+import Master_paper_home_foto from '../assets/2_Master_paper_home.png'
+import Real_estate_overview_foto from '../assets/3_Real_estate_overview.png'
+import Gym_search_foto from '../assets/4_Gym_update.png'
+import Christmas_sweet_home_foto from '../assets/5_Christmas_sweet_home.png'
+
 const Home = () => {
 
-    return (
+    const projects_fotos = [
+        Blog_blogs_foto,
+        Master_paper_home_foto,
+        Real_estate_overview_foto,
+        Gym_search_foto,
+        Christmas_sweet_home_foto
+      ]
 
-        <div>Hello from Home</div>
+      const containerStyle = {
+        width: '90%', // 70%
+        height: '350px', // 480px
+        margin: '0 auto', // maybe not necessary
+        //border: 'solid black 3px',
+        //borderRadius: '5px',
+      }
+
+    /*
+    return (
+        <>
+            <div>Hello from Home</div>
+
+            <div style={containerStyle}>
+                <ImageSlider projects_fotos={projects_fotos} parentWidth={1500} />  
+            </div>
+        </>
+    )
+    */
+    return (
+        <>
+            <div>Hello from Home</div>
+
+            <div style={containerStyle}>
+                <ImageSlider projects_fotos={projects_fotos} parentWidth={1500} />  
+            </div>
+        </>
     )
 }
 
---
+++ frontend/src/components/ImageSlider.jsx
@@ -0,0 +1,183 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+import { Link } from 'react-router-dom'
+
+// credit to monsterlessonsacademy (GitHub Account) with the ImageSlider
+
+const ImageSlider = ({ projects_fotos, parentWidth }) => {
+
+    const timeRef = useRef(null);
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const sliderStyles = {
+        height: '100%',
+        position: 'relative',
+    };
+
+    
+    const slidesContainerStyles = {
+        display: 'flex',
+        height: '100%',
+        transition: 'transform ease-out 0.6s',
+    }
+
+    
+    const getSlidesContainerStylesWithWidth = () => ({
+        ...slidesContainerStyles,
+        width: parentWidth * projects_fotos.length,
+        transform: `translateX(${-(currentIndex * parentWidth)}px)`
+    })
+    
+
+    const slideStyles = {
+        width: '100%',
+        height: '100%',
+        borderRadius: '10px',
+        backgroundSize: 'cover',
+        backgroundImage: `url(${projects_fotos[currentIndex]})`,
+    };
+
+    const getSlidesStylesWithBackground = (slideIndex) => ({
+        ...slideStyles,
+        //backgroundImage: `url(${projects_fotos[slideIndex]})`,
+    });
+
+    
+    const leftArrowStyles = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        left: '32px',
+        fontSize: '45px',
+        color: '#83677B',
+        zIndex: 1,
+        cursor: 'pointer'
+    };
+    
+    const rightArrowStyles = {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate(0, -50%)',
+        right: '32px',
+        fontSize: '45px',
+        color: '#83677B',
+        zIndex: 1,
+        cursor: 'pointer'
+    };
+    
+
+    const goToPrevious = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? projects_fotos.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const goToNext = useCallback(() => {
+        const isLastSlide = currentIndex === projects_fotos.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }, [currentIndex, projects_fotos]);
+    
+
+    
+    const dotsContainerStyles = {
+        display: 'flex',
+        justifyContent: 'center',
+        transform: 'translate(0, -100%)',
+    };
+    
+    const dotStyles = {
+        margin: '5px 3px',
+        cursor: 'pointer',
+        fontSize: '15px',
+        color: '#83677B',
+    };
+    
+    const padding_link = {
+        fontSize: 20,
+        textDecoration: "none",
+        fontWeight: 'bold',
+    }
+
+    const view_project_style = {
+        position: 'absolute',
+        padding: '7px',
+        transform: 'translate(0, -35%)',
+        color: 'white',
+        zIndex: 1,
+        cursor: 'pointer',
+        border: 'solid white 2px',
+        borderRadius: '10px',
+        backgroundColor: '#83677B'
+    };
+
+    const center_view_project_style = {
+        position: 'absolute',
+        top: '48%',
+        left: '45%',
+      }
+
+    const goToSlide = slideIndex => {
+        setCurrentIndex(slideIndex)
+    };
+    
+
+    const slidesContainerOverflowStyles = {
+        overflow: 'hidden',
+        height: '100%'
+    }
+
+    
+    useEffect(() => {
+        if (timeRef.current) {
+            clearTimeout(timeRef.current)
+        }
+        timeRef.current = setTimeout(() => {
+            goToNext();
+        }, 3000);
+
+        return () => clearTimeout(timeRef.current)
+    }, [goToNext]);
+    
+
+    return (
+
+
+        <div style={sliderStyles}>
+
+            <div style={leftArrowStyles} onClick={goToPrevious}> ❮ </div>
+            <div style={rightArrowStyles} onClick={goToNext}> ❯ </div>
+
+            <div style={slidesContainerOverflowStyles}>
+
+            <Link style={padding_link} to={`/projects/${currentIndex + 1}`}>
+
+                <div style={getSlidesContainerStylesWithWidth()}>
+                    {projects_fotos.map((_, slideIndex) => (
+                        <div key={slideIndex} style={getSlidesStylesWithBackground(slideIndex)}> </div>
+                    ))}
+                </div>
+
+                <div style={center_view_project_style}>
+                    <div style={view_project_style}>
+                        View
+                    </div>
+                </div>
+
+            </Link>
+
+                <div style={dotsContainerStyles}>
+                    {projects_fotos.map((slide, slideIndex) => (
+                    <div key={slideIndex} style={dotStyles} onClick={() => goToSlide(slideIndex)} >⬤</div> 
+                    )
+                    )}
+                </div>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default ImageSlider
