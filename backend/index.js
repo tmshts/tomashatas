b108862d2909cb16107d45bd34f5f8c5a6bd4b8b
tmshts <tomhatas@gmail.com>
2024-07-28 22:40:43 -0500

    update before fly deploy

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,16 @@ const app = require('./app') // the actual Express application
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
+
+const PORT = config.PORT || 3001
+app.listen(PORT, () => {
+  //console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
+})
+
+/*
 //const PORT = 3003
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
 })
+*/
